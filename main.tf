8d475103d5bf41e0978eded4629c4ad8c6707b76
Commander_Cobra <gregawscsa@gmail.com>
2021-07-27 16:51:14 -0400

    fixed tf cloud

--- main.tf
+++ main.tf
@@ -1,15 +1,6 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
   backend "remote" {
-   organization = "Cobra-Net"
+    organization = "Cobra-Net"
 
     workspaces {
       name = "100_Days_Of_Go"
