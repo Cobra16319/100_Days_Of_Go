1c88512d91b047c45e6d7a7238f5a1a5bfe6aac3
Commander_Cobra <gregawscsa@gmail.com>
2021-07-27 18:36:04 -0400

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-# Updated with guide to test CI
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
-  backend "remote" {
-   organization = "Cobra-Net"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
-}
+#     # The configuration for the `remote` backend.
+      terraform {
+        backend "remote" {
+#         # The name of your Terraform Cloud organization.
+          organization = "Cobra-Net"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+          workspaces {
+            name = "Go"
+          }
+        }
+      }
+#
+#     # An example resource that does nothing.
+      resource "null_resource" "example" {
+        triggers = {
+          value = "A example resource that does nothing!"
+        }
+      }
