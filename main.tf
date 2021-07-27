1948c542041746935a8f068800a750a3ce7023d4
Commander_Cobra <gregawscsa@gmail.com>
2021-07-27 15:40:11 -0400

    fixing CI for TF

---
+++ main.tf
@@ -0,0 +1,21 @@
+#     
+#     For testing purpose only. Production costs too $ 
+#     The configuration for the `remote` backend.
+#     terraform {
+        backend "remote" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "Cobra-Net"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "100_Days_of_GO"
+#         }
+#       }
+#     }
+#
+#     # An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
