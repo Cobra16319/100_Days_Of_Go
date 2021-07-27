bc53ee65c6a2984a9e26e44ab51e48b1f3f81969
Commander_Cobra <gregawscsa@gmail.com>
2021-07-27 17:15:30 -0400

    updated correct main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-# Just for CI testing do not use.
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
   backend "remote" {
-    organization = "Cobra-Net"
+   organization = "Cobra-Net"
 
     workspaces {
       name = "100_Days_Of_Go"
