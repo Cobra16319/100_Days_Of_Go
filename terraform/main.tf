82b16eecf2265bc2f9837681fce7a2345cb2ed78
Commander_Cobra <gregawscsa@gmail.com>
2021-07-27 15:21:54 -0400

    updated sensitive values in tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@
 provider "aws" {
   region = "us-west-2"
 
-  access_key                  = "anaccesskey"
-  secret_key                  = "asecretkey"
+  access_key                  = "sensitive"
+  secret_key                  = "sensitive"
   s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
