3f02222f270ccf49d33059a3340b0ee58c902621
Commander_Cobra <gregawscsa@gmail.com>
2021-07-28 10:52:45 -0400

    Initial test of terraform for HCP on AWS with Consul

---
+++ terraform/consul-hcp/consul.tf
@@ -0,0 +1,12 @@
+resource "hcp_hvn" "example_hvn" {
+  hvn_id         = var.hvn_id
+  cloud_provider = var.cloud_provider
+  region         = var.region
+}
+
+resource "hcp_consul_cluster" "example_hcp" {
+  hvn_id          = hcp_hvn.example_hvn.hvn_id
+  cluster_id      = var.cluster_id
+  tier            = "development"
+#   public_endpoint = true
+}
